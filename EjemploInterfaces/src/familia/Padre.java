f0660908e3bbf243c2c36950a2d1aed0666374fc
augustosalazar@uninorte.edu.co <Be2l2000>
2016-03-18 07:14:58 -0500

    Nuevo ejemplo de uso de interfaces

---
+++ EjemploInterfaces/src/familia/Padre.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package familia;
+
+/**
+ *
+ * @author augustosalazar
+ */
+public class Padre extends MiembroFamilia implements MiembroFamilia.AdministrarPerro{
+
+    @Override
+    public void sacarPerro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void bañarPerro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alimentarPerro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void consentirPerro() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
