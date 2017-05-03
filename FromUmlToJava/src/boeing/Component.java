40000690d6c15c694bb5fc13b3b3c2faa89f5b12
Augusto Salazar Silva <augustosalazar@uninorte.local>
2017-05-03 18:23:42 -0500

    From Uml to code

---
+++ FromUmlToJava/src/boeing/Component.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boeing;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author augustosalazar
+ */
+public class Component {
+    
+    private ArrayList<Component> assembly;
+    private ArrayList<Component> subAssembly;
+    private Airplane airplane;
+
+    public Component(Airplane airplane) {
+        this.airplane = airplane;
+        subAssembly = new ArrayList<>();
+        Component subx = new Component(airplane);
+        Component suby = new Component(airplane);
+        subAssembly.add(subx);
+        subAssembly.add(suby);
+        assembly = new ArrayList<>();
+    }
+    
+    public void addAssembly(Component component){
+        assembly.add(component);
+    }
+    
+
+    
+    
+}
